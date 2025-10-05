a4fb4e576882cda55844c8df0b0cbb530d3279f1
Scott Addie <scott.addie@microsoft.com>
2025-10-05 13:14:08 -0500

    Add DAC demo 4

--- DefaultAzureCredential/Program.cs
+++ DefaultAzureCredential/Program.cs
@@ -1,5 +1,3 @@
-#define Demo1
-
 using Microsoft.Extensions.Azure;
 using IdentityPlayground.WebApi;
 using Azure.Identity;
@@ -8,19 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAzureClients(configureClients: c =>
 {
     IConfigurationSection keyVaultConfig = builder.Configuration.GetSection("Azure:KeyVault");
-
-#if Demo1
-    c.AddSecretClient(keyVaultConfig);
-#elif Demo2
-    c.AddSecretClient(keyVaultConfig);
-    //.WithCredential(new DefaultAzureCredential(DefaultAzureCredential.DefaultEnvironmentVariableName));
-#elif Demo3
-    ChainedTokenCredential credential = new(
-        new AzureCliCredential(),
-        new VisualStudioCredential());
-    c.AddSecretClient(keyVaultConfig)
-        .WithCredential(credential);
-#elif Demo4
     IConfigurationSection storageConfig = builder.Configuration.GetSection("Azure:Storage");
 
     c.AddSecretClient(keyVaultConfig);
@@ -28,7 +13,6 @@ builder.Services.AddAzureClients(configureClients: c =>
 
     c.UseCredential(new DefaultAzureCredential(
         DefaultAzureCredential.DefaultEnvironmentVariableName));
-#endif
 });
 
 var app = builder.Build();
